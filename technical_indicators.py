2c6cde3a63173cf549225d6dba3cbcf3fd39254c
ryan-kttam <ryantam0626@gmail.com>
2020-07-29 11:44:29 -0700

    added ADX indicator

--- technical_indicators.py
+++ technical_indicators.py
@@ -33,3 +33,50 @@ def rsi(data, look_back_period=14):
     return rsi
 
 
+def smoothing(data, look_back_period):
+    # applying this equation: y_t = (1-a)*y_(t-1) + x_t/n, where a = 1/n
+    # this is equivalent to wilder smoothing
+    result = data.copy()
+    result = result.rolling(look_back_period).mean()
+    result[look_back_period:] = data[look_back_period:]
+    return result.ewm(alpha=1 / look_back_period, adjust=False).mean()
+
+
+def adx(close, high, low, look_back_period, modified_version=False):
+    # This function will return two dataframes: ADX and (plus_di-minus_di)
+
+    # calculate the true range
+    tr = pd.DataFrame(index=close.index)
+    tmp = pd.concat([high - low,
+                     abs(high - close.shift(1)),
+                     abs(low - close.shift(1))], axis=1)
+    for i in close.columns:
+        tr[i] = tmp[i].max(axis=1)
+
+    direction_movement_high = high - high.shift(1)
+    direction_movement_low = low.shift(1) - low
+    direction_movement_high[direction_movement_high < 0] = 0
+    direction_movement_low[direction_movement_low < 0] = 0
+    direction_movement_high[~(direction_movement_high > direction_movement_low)] = 0
+    direction_movement_low[~(direction_movement_low > direction_movement_high)] = 0
+
+    smoothed_tr = smoothing(tr, look_back_period)
+    smoothed_dmh = smoothing(direction_movement_high, look_back_period)
+    smoothed_dml = smoothing(direction_movement_low, look_back_period)
+
+    plus_di = smoothed_dmh / smoothed_tr * 100
+    minus_di = smoothed_dml / smoothed_tr * 100
+
+    # Calculating DX and ADX
+    dx = abs(plus_di - minus_di) / abs(plus_di + minus_di) * 100
+    adx = smoothing(dx, look_back_period)
+    signals = (plus_di - minus_di)
+
+    if modified_version:
+        signals_2 = signals.copy()
+        for i in signals.columns:
+            signals[i][signals[i] > 0] = 1
+            signals[i][signals[i] < 0] = -1
+        return (adx * signals), signals_2
+    else:
+        return adx, signals
